a3c3d8491016798a4315911a4db74c0a4a83e8ce
Mina Magdy <71466151+MiinaMagdy@users.noreply.github.com>
2024-02-01 18:16:41 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,34 +1,39 @@
 import subprocess
 import random
 
-# DO NOT FORGET: freopen("test.in", "r", stdin);
-# Make Sure: TO BE IN THE SAME DIRECTORY or Change the path below
+def is_correct(myRes, correctRes, inputFile):
+    return myRes == correctRes
+
+
+## DO NOT FORGET: freopen("test.in", "r", stdin);
+## Make Sure: TO BE IN THE SAME DIRECTORY or Change the path below
+subprocess.run(["g++", "myCode.cpp", "-o", "myCode"])
+subprocess.run(["g++", "anotherCode.cpp", "-o", "anotherCode"])
 
 while True:
-    # INPUTS use random module
-
-
-    # write INPUTS in test.in
+    ## write INPUTS in test.in
     with open('test.in', 'w') as file:
-        # use write method and cast inputs to str()
-        
+        ## INPUTS use random module
+        ## use this function to random.randint
+        ## use write method and cast inputs to str()
+        ## uncomment next line if there are testcases
+        # t = random.randint(1, 1); file.write(str(t) + '\n')
+        pass
 
-    # DO NOT CHANGE ANY THING BELOW!!!
+    ## DO NOT CHANGE ANY THING BELOW!!!
+    myRes = subprocess.run(["./myCode"], capture_output=True).stdout.decode()
+    anotherRes = subprocess.run(["./anotherCode"], capture_output=True).stdout.decode()
+
+    ## Displaying
+    print("\n\nTest:")
     with open('test.in', 'r') as file:
         for line in file:
             print(line, end='')
-    
-    print("\n\nTesting...")
 
-    subprocess.run(["g++", "myCode.cpp", "-o", "myCode"])
-    subprocess.run(["g++", "anotherCode.cpp", "-o", "anotherCode"])
-    myRes = subprocess.run(["./myCode", "<", "test.in"], capture_output=True).stdout.decode()
-    anotherRes = subprocess.run(["./anotherCode", "<", "test.in"], capture_output=True).stdout.decode()
-    print("myCode: ")
+    print("\n\nMy Code's Output: ")
     print(myRes)
-    print("anotherCode: ")
+    print("Your Code's Output: ")
     print(anotherRes)
-    if myRes != anotherRes:
+    # checker below
+    if not is_correct(myRes, anotherRes, 'test.in'):
         break
-
-
